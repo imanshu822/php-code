193938c15b7fc3258ebe8dddefe7c5826de7d2e4
Anshu <imanshu822@gmail.com>
2024-02-17 10:52:31 +0530

    files are added

--- app/index.php
+++ app/index.php
@@ -1,3 +1,16 @@
 <?php
-echo "Hello Anubhav";
+// Set the content type to JSON
+header('Content-Type: application/json');
+
+// Check if the request method is GET
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $response = array(
+        'message' => 'Hello, World!'
+    );
+    echo json_encode($response);
+} else {
+    // If the request method is not GET, return an error response
+    http_response_code(405); // Method Not Allowed
+    echo json_encode(array('error' => 'Method Not Allowed'));
+}
 ?>
